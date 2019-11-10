7cca22dcb87b91014fb2bb89dd2eece0610bc47f
milesswu <mileswu13@gmail.com>
2019-11-09 19:11:13 -0800

    Styles for node widgets
    Modifications of node widgets, both interms of style and content. These
    are made for presentational purposes.

--- frontend/src/app/pages/StoryBuilder/StoryNodeWidget.tsx
+++ frontend/src/app/pages/StoryBuilder/StoryNodeWidget.tsx
@@ -1,45 +1,67 @@
-import * as React from 'react';
-import { DiagramEngine, PortWidget } from '@projectstorm/react-diagrams-core';
-import { StoryNode } from './StoryNode';
+import * as React from "react";
+import { DiagramEngine, PortWidget } from "@projectstorm/react-diagrams-core";
+import { StoryNode } from "./StoryNode";
 
 export interface TSCustomNodeWidgetProps {
 	node: StoryNode;
 	engine: DiagramEngine;
-	callback: (node: StoryNode) => void
+	callback: (node: StoryNode) => void;
 }
 
 export interface TSCustomNodeWidgetState {
-	callback: (node: StoryNode) => void
+	callback: (node: StoryNode) => void;
 }
 
-export class TSCustomNodeWidget extends React.Component<TSCustomNodeWidgetProps, TSCustomNodeWidgetState> {
+export class TSCustomNodeWidget extends React.Component<
+	TSCustomNodeWidgetProps,
+	TSCustomNodeWidgetState
+> {
 	constructor(props: TSCustomNodeWidgetProps) {
 		super(props);
-		this.state = {callback: props.callback};
+		this.state = { callback: props.callback };
 	}
 
 	render() {
 		var outputPorts = this.props.node.getOutputPorts();
-		const outputs = outputPorts.map((value) => 
-			<PortWidget engine={this.props.engine} port={value} key={value.getID()}>
-				<div className="circle-port"/>
-			</PortWidget>
-		);
+		const outputs = outputPorts.map((value) => (
+			<div className="output-port">
+				<PortWidget
+					engine={this.props.engine}
+					port={value}
+					key={value.getID()}
+				>
+					<div className="circle-port" />
+				</PortWidget>
+				<p className="port-answer">{value.answer}</p>
+			</div>
+		));
 		var input = this.props.node.getInputPort();
 		let inelement;
-		if(input){
-			inelement = <PortWidget engine={this.props.engine} port={input} key={input.getID()}>
-			<div className="circle-port"/>
-		</PortWidget>;
-		}else{
+		if (input) {
+			inelement = (
+				<PortWidget
+					engine={this.props.engine}
+					port={input}
+					key={input.getID()}
+				>
+					<div className="circle-port" />
+				</PortWidget>
+			);
+		} else {
 			inelement = null;
 		}
 		return (
-			<div className={this.props.node.isBeginning ? 'story-header story-node' : 'story-node'}
-			 onClick={() => (this.state.callback(this.props.node))}>
-				{inelement}
+			<div
+				className={
+					this.props.node.isBeginning
+						? "story-header story-node"
+						: "story-node"
+				}
+				onClick={() => this.state.callback(this.props.node)}
+			>
+				<div className="port-container">{inelement}</div>
 				<p>{this.props.node.getShortText()}</p>
-				{outputs}
+				<div className="port-container">{outputs}</div>
 			</div>
 		);
 	}
