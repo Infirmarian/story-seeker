474c76687ae1e00ec9147747896bc26585fca929
Robert Geil <robert@rgeil.net>
2019-11-12 04:06:05 -0800

    Added some final functionality for Alexa

--- alexa/utils.py
+++ alexa/utils.py
@@ -27,22 +27,22 @@ def create_presigned_url(object_name):
 
 def select_option(attr, option):
     story = attr['STORY']
-    index = attr['INDEX']
+    index = attr['STORY_STACK'][-1]
     if option > len(story['content'][index]['options']):
         return 'Please choose an option between 1 and %d' % len(story['content'][index]['options'])
-    attr['INDEX'] = story['content'][index]['options'][option-1][1]
-    return get_main(attr) + ' ' + get_question(attr)
+    attr['STORY_STACK'].append(story['content'][index]['options'][option-1][1])
+    return '%s\n%s' % (get_main(attr), get_question(attr))
 
 def get_initial(attr):
     return 'Ok, lets start %s. %s %s' % (attr['STORY']['title'], get_main(attr), get_question(attr))
 def get_main(attr):
-    return attr['STORY']['content'][attr['INDEX']]['main']
+    return attr['STORY']['content'][attr['STORY_STACK'][-1]]['main']
 def get_question(attr):
-    if 'options' not in attr['STORY']['content'][attr['INDEX']]:
+    if 'options' not in attr['STORY']['content'][attr['STORY_STACK'][-1]]:
         attr['STATE'] = lambda_function.STORY_ENDED
-        return 'The End. Do you want to try this story again?'
-    options = [n[0] for n in attr['STORY']['content'][attr['INDEX']]['options']]
-    result ='%s %s %s' % (attr['STORY']['content'][attr['INDEX']]['question'],
+        return 'The End. Do you want to go back, or restart, or exit?'
+    options = [n[0] for n in attr['STORY']['content'][attr['STORY_STACK'][-1]]['options']]
+    result ='%s %s %s' % (attr['STORY']['content'][attr['STORY_STACK'][-1]]['question'],
                     '\n'.join('%s: %s,' % (i+1, options[i]) for i in range(len(options)-1)),
                     ' or \n%s: %s' % (len(options), options[-1]))
     return result
@@ -75,7 +75,7 @@ def format_list(items):
     if len(items) == 1:
         return items[0]
     if len(items) == 2:
-        return '%s and %s' %(items[0],items[1])
+        return '%s, and %s' %(items[0],items[1])
     return '%s, and %s' % (', '.join(items[:-1]), items[-1])
 
 def product_format(products):
@@ -84,3 +84,14 @@ def product_format(products):
         return product_names[0]
     else:
         return '%s %s' % (', '.join(product_names[:-1]), product_names[-1])
+
+def get_credit_count(handler_input):
+    # HandlerInput -> int
+    products = in_skill_product_response(handler_input).in_skill_products
+    total = 0
+    for item in products:
+        number = item.active_entitlement_count
+        if item.reference_name == 'story_token_5':
+            number *= 5
+        total += number
+    return total
