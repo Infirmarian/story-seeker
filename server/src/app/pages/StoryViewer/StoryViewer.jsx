0778361d9d06a7bfd14a9fb9a0fe5a3cdeaaa731
milesswu <mileswu13@gmail.com>
2019-11-18 23:28:53 -0800

    Styling Story Viewer and Details
    Adding styling and functionality to the story details and story viewer
    pages. Getting ready for eventual server data.

--- server/src/app/pages/StoryViewer/StoryViewer.jsx
+++ server/src/app/pages/StoryViewer/StoryViewer.jsx
@@ -3,47 +3,48 @@ import StoryCard from "./StoryCard/StoryCard";
 import "./StoryViewer.css";
 
 function StoryViewer() {
-  const [stories, setStories] = useState([]);
-  useEffect(() => {
-    // fetch user data
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-        const userStories = data
-          .filter(el => {
-            return el.userId === 1;
-          })
-          .slice(0, 5);
-        console.log(userStories);
-        setStories(userStories);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-    return () => {
-      setStories([]);
-    };
-  }, []);
+	const [stories, setStories] = useState([]);
+	useEffect(() => {
+		// fetch user data
+		fetch("https://jsonplaceholder.typicode.com/posts")
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data);
+				const userStories = data
+					.filter((el) => {
+						return el.userId === 1;
+					})
+					.slice(0, 5);
+				console.log(userStories);
+				setStories(userStories);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+		return () => {
+			setStories([]);
+		};
+	}, []);
 
-  return (
-    <div className="Story-Viewer">
-      <div className="card-container">
-        {stories.map(story => {
-          return (
-            <StoryCard
-              key={story.id}
-              id={story.id}
-              title={story.title}
-              description={story.body}
-            />
-          );
-        })}
-      </div>
-    </div>
-  );
+	return (
+		<div className="Story-Viewer">
+			<div className="card-container">
+				{stories.map((story) => {
+					return (
+						<StoryCard
+							key={story.id}
+							id={story.id}
+							title={story.title}
+							description={story.body}
+							publishStatus={"Not Yet Published"}
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default StoryViewer;
