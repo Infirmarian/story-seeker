d83052a525f27ac74d828675d295c292b12a706a
milesswu <mileswu13@gmail.com>
2019-12-16 17:31:13 -0800

    update

---
+++ server/src/app/pages/StoryBuilder/StoryModel.js
@@ -0,0 +1,66 @@
+import {
+  DiagramModel,
+  LinkModel,
+  DefaultLinkModel
+} from "@projectstorm/react-diagrams";
+import { StoryNode } from "./StoryNode";
+
+class StoryModel extends DiagramModel {
+  storyID;
+  serialize() {
+    let result = {
+      zoom: this.getZoomLevel(),
+      offsetX: this.getOffsetX(),
+      offsetY: this.getOffsetY(),
+      nodes: [],
+      links: []
+    };
+    this.getNodes().forEach(e => {
+      result.nodes.push(e.serializeNode());
+    });
+    this.getLinks().forEach(l => {
+      result.links.push({
+        sourceID: l
+          .getSourcePort()
+          .getNode()
+          .getID(),
+        sourceIndex: l
+          .getSourcePort()
+          .getNode()
+          .getPortIndex(l.getSourcePort().getID()),
+        sink: l
+          .getTargetPort()
+          .getNode()
+          .getID()
+      });
+    });
+    console.log(result);
+    return JSON.stringify(result);
+  }
+
+  deserializeModel(obj, engine) {
+    console.log(obj);
+    this.setZoomLevel(obj.zoom);
+    this.setOffsetX(obj.offsetX);
+    this.setOffsetY(obj.offsetY);
+    let nodes = {};
+    obj.nodes.forEach(e => {
+      let n = new StoryNode({ text: "", engine });
+      n.deserializeNode(e);
+      nodes[n.getID()] = n;
+      this.addNode(n);
+    });
+    obj.links.forEach(l => {
+      let ln = new DefaultLinkModel();
+      let sink = nodes[l.sink].getInputPort();
+      ln.setTargetPort(sink);
+      let source = nodes[l.sourceID].getOutPorts()[l.sourceIndex];
+      if (source) {
+        ln.setSourcePort(source);
+        this.addLink(ln);
+      }
+    });
+  }
+}
+
+export default StoryModel;
