ac2170ccf6ebd164b9dec9f72c888d805ac4a8b3
milesswu <mileswu13@gmail.com>
2019-12-17 18:20:38 -0800

    Story Details page styling
    Changed styling for story details page. Layout is more formal and
    polished. Added pricing options.

--- server/src/app/pages/StoryDetails/StoryDetails.jsx
+++ server/src/app/pages/StoryDetails/StoryDetails.jsx
@@ -10,19 +10,19 @@ function SaveStoryContent(content, id, history) {
     fetch(URL + "/api/overview/" + id, {
       method: "PUT",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: content
+      body: content,
     });
   } else {
     fetch(URL + "/api/overview", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
-      body: content
-    }).then(response => {
-      response.json().then(json => {
+      body: content,
+    }).then((response) => {
+      response.json().then((json) => {
         if (response.status === 201) {
           console.log("Successfully created");
           const id = json.id;
@@ -41,12 +41,12 @@ function DeleteStory(id, history) {
     )
   ) {
     fetch(URL + "/api/overview/" + id, {
-      method: "DELETE"
-    }).then(response => {
+      method: "DELETE",
+    }).then((response) => {
       if (response.status === 200) {
         history.push("/viewer");
       } else {
-        response.json().then(json => {
+        response.json().then((json) => {
           console.error(json);
         });
       }
@@ -56,24 +56,34 @@ function DeleteStory(id, history) {
 function StoryDetails(props) {
   let history = useHistory();
   const { id } = props.match.params;
+
+  // Data for current story being viewed
   const [storyDetails, setStoryDetails] = useState({});
   useEffect(() => {
     if (id) {
-      fetch(URL + "/api/overview/" + id).then(response => {
-        response.json().then(json => {
+      fetch(URL + "/api/overview/" + id).then((response) => {
+        response.json().then((json) => {
           setStoryDetails({
             title: json.title,
             summary: json.summary,
             genre: json.genre,
+            price: json.price,
             published: json.published,
-            last_modified: json.last_modified
+            last_modified: json.last_modified,
           });
         });
       });
     }
   }, [id]);
+  const {
+    title,
+    summary,
+    genre,
+    price,
+    published,
+    last_modified,
+  } = storyDetails;
 
-  const { title, summary, genre, published, last_modified } = storyDetails;
   const editStoryButton = id ? (
     <Link to={"/builder/" + id} className={"btn btn-primary "}>
       Edit Story
@@ -82,27 +92,27 @@ function StoryDetails(props) {
   const deleteStoryButton = id ? (
     <button
       className="btn btn-alert"
-      onClick={event => {
+      onClick={(event) => {
         event.preventDefault();
         DeleteStory(id, history);
         return false;
       }}
     >
-      Delete
+      Delete Story
     </button>
   ) : null;
   const submitStoryButton = id ? (
     <button
       className="btn btn-primary"
-      onClick={event => {
+      onClick={(event) => {
         event.preventDefault();
         const s = window.confirm(
           "Are you sure you want to submit? You cannot make any more changes once the story has been submitted for approval"
         );
         if (!s) return false;
         fetch(URL + `/api/submit/${id}`, {
-          method: "POST"
-        }).then(response => {
+          method: "POST",
+        }).then((response) => {
           if (response.status !== 200) {
             window.alert("Failed to submit for approval");
           }
@@ -110,21 +120,21 @@ function StoryDetails(props) {
         return false;
       }}
     >
-      Submit for Approval
+      Submit Story for Approval
     </button>
   ) : null;
   return (
-    <div>
+    <div className="Story-Details">
       <Navbar />
       <form
-        className="Story-Details"
-        onSubmit={event => {
+        className="details-form"
+        onSubmit={(event) => {
           event.preventDefault();
           SaveStoryContent(
             JSON.stringify({
               title: event.target.title.value,
               summary: event.target.summary.value,
-              genre: event.target.genre.value
+              genre: event.target.genre.value,
             }),
             id,
             history
@@ -132,61 +142,97 @@ function StoryDetails(props) {
           return false;
         }}
       >
-        <label htmlFor="title">Title</label>
-        <input
-          className="form-control"
-          id="title"
-          type="text"
-          name="title"
-          defaultValue={title}
-        />
-        <label htmlFor="summary">Summary</label>
-        <textarea
-          className="form-control"
-          id="summary"
-          type="text"
-          defaultValue={summary}
-        />
-        <label htmlFor="genre">Genre</label>
-        <select
-          id="genre"
-          className="form-control form-control-sm"
-          value={genre}
-          onChange={event => {
-            console.log(event.target.value);
-            setStoryDetails({ title, summary, genre: event.target.value });
-          }}
-        >
-          <option value="adventure">Adventure</option>
-          <option value="comedy">Comedy</option>
-          <option value="fantasy">Fantasy</option>
-          <option value="science fiction">Science Fiction</option>
-          <option value="western">Western</option>
-          <option value="romance">Romance</option>
-          <option value="mystery">Mystery</option>
-          <option value="detective">Detective</option>
-          <option value="dystopia">Dystopia</option>
-        </select>
-        <div
-          className={
-            published
-              ? `publish-status publish-status-${published}`
-              : "publish-status"
-          }
-        >
-          {published
-            ? published === "not published"
-              ? "not\u00a0published"
-              : published
-            : "not\u00a0published"}
+        <div className="info">
+          <label className="input-labels" htmlFor="title">
+            Title
+          </label>
+          <input
+            className="form-control"
+            id="title"
+            type="text"
+            name="title"
+            defaultValue={title}
+          />
+          <label className="input-labels" htmlFor="summary">
+            Summary
+          </label>
+          <textarea
+            className="form-control"
+            id="summary"
+            type="text"
+            defaultValue={summary}
+          />
+          <label className="input-labels" htmlFor="genre">
+            Genre
+          </label>
+          <select
+            id="genre"
+            className="form-control form-control-sm"
+            value={genre}
+            onChange={(event) => {
+              console.log(event.target.value);
+              setStoryDetails({
+                title,
+                summary,
+                genre: event.target.value,
+                published,
+                last_modified,
+              });
+            }}
+          >
+            <option value="adventure">Adventure</option>
+            <option value="comedy">Comedy</option>
+            <option value="fantasy">Fantasy</option>
+            <option value="science fiction">Science Fiction</option>
+            <option value="western">Western</option>
+            <option value="romance">Romance</option>
+            <option value="mystery">Mystery</option>
+            <option value="detective">Detective</option>
+            <option value="dystopia">Dystopia</option>
+          </select>
+          <label htmlFor="price" className="input-labels">
+            Pricing
+          </label>
+          <select
+            name=""
+            id="price"
+            className="form-control form-control-sm"
+            value={price}
+            onChange={(event) => {
+              setStoryDetails({
+                title,
+                summary,
+                genre,
+                price: event.target.value,
+                published,
+                last_modified,
+              });
+            }}
+          >
+            <option value="0">free</option>
+            <option value="0.99">$0.99</option>
+          </select>
         </div>
-        <div>Last Modified: {last_modified}</div>
-        <button type="submit" className="btn btn-primary">
-          Save
-        </button>
-        {editStoryButton}
-        {submitStoryButton}
-        {deleteStoryButton}
+        <div className="uneditable-info">
+          <div className={`status ${published ? "status-success" : ""}`}>
+            {published
+              ? published === "not published"
+                ? "not\u00a0published"
+                : published
+              : "not\u00a0published"}
+          </div>
+          <div className="status status-primary">
+            Last Modified: {last_modified}
+          </div>
+        </div>
+        <div className="button-section">
+          <button type="submit" className="btn btn-primary ml-0">
+            Save Details
+          </button>
+          {editStoryButton}
+          {deleteStoryButton}
+          {submitStoryButton}
+        </div>
       </form>
     </div>
   );
