8b7e8716656853e88a21e12c37a3553c01f1b329
Miles Wu <mileswu13@gmail.com>
2020-01-01 19:55:00 -0800

    Add comments for Button Component usage

--- server/src/app/components/Button/Button.jsx
+++ server/src/app/components/Button/Button.jsx
@@ -3,7 +3,16 @@ import { Link } from "react-router-dom";
 import "./Button.css";
 
 function Button(props) {
-	const { variant, type, onClick, disabled, link, href } = props;
+	// Button usage: <Button {...props} >{children}</Button>
+	// variant (TODO if necessary): specifies the type of button to be rendered; see material-UI button variants
+	// color: specifies color theme of button; 'primary' | 'secondary' | 'alert'
+	// size: specifies padding size of button; sm | md | lg | xl
+	// type: specifies defauly HTML element button type; 'button' | 'reset' | 'submit'
+	// onClick: specifies onClick event handler
+	// disabled: specifies whether button is disabled; true | false
+	// link: pass in the route that the button should navigate to on click
+	// href: use for internal page navigation (jump to element with id)
+	const { variant, color, size, type, onClick, disabled, link, href } = props;
 
 	const Component = props.component || href ? "a" : link ? Link : "button";
 
@@ -16,18 +25,20 @@ function Button(props) {
 	if (href) attributes.href = href;
 
 	const btnVariant = variant ? `btn-${variant}` : "btn";
-	const color = disabled
+	const btnColor = disabled
 		? "btn-disabled"
-		: props.color
-		? `btn-${props.color}`
+		: color
+		? `btn-${color}`
 		: "btn-primary";
-	const size = props.size ? `btn-${props.size}` : "";
+	const btnSize = size ? `btn-${size}` : "";
 	const first = props.first ? "ml-0" : "";
 
-	const classes = [btnVariant, color, size, first, props.className].join(" ");
+	const classes = [btnVariant, btnColor, btnSize, first, props.className].join(
+		" ",
+	);
 
 	return (
-		<Component style={{}} className={classes} {...attributes}>
+		<Component className={classes} {...attributes}>
 			<span>{props.children}</span>
 		</Component>
 	);
