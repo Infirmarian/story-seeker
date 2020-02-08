367e03fb71f4bd6aefa0ff8e8dc0982b7639dc75
Robert Geil <robert@rgeil.net>
2020-02-08 14:51:44 -0800

    Added dev build scripts

--- server/src/utils/constants.js
+++ server/src/utils/constants.js
@@ -1,3 +1,6 @@
-//export const URL = "http://localhost:5000";
-export const URL = "https://www.storyseeker.fun";
-//THIS IS A COMMENT PLEASE WORK
+var defURL;
+defURL = "https://www.storyseeker.fun";
+if (process.env.SERVER_STATE === "DEVELOPMENT") {
+  defURL = "http://localhost:5000";
+}
+export const URL = defURL;
